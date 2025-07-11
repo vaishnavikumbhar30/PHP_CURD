99d2fec2da0e7e418346d5ff2bbb4ec6be66af9c
Vaishnavi Kumbhar <148613061+vaishnavikumbhar30@users.noreply.github.com>
2025-07-11 10:44:36 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP-CURD</title>
+    <title> PHP - CURD</title>
 
     <!-- bootstrap css -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
